bf619cbca318f079f893f0426d6b724265ed4bd6
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-14 21:04:56 +0500

    Update NavBar.tsx

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -1,26 +1,53 @@
 'use client'
+// import React from 'react';
+// import Link from 'next/link';
+// import { useRouter } from 'next/router';
+// function HandleClick() {
+//   const Router = useRouter();
+//   Router.push('/');
+
+// }
+
+// const NavBar=()=>(
+  
+// <div className="mx-auto w-auto fix top-0 z-500">
+//     <div className="bg-slate-700 h-auto  flex flex-col z-100 items-center text-2xl font-semibold text-white pb-10 sm:flex-row justify-evenly">
+//     {/*NavBar Links */}
+        
+//         <Link href='#about' onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
+//         <Link href="#skills" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
+//         <Link href="#education" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
+//         <Link href="#interests" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
+//         <Link href="#goals" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
+
+//       </div></div>)
+  
+
+// export default NavBar;
+
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-function HandleClick() {
-  const Router = useRouter();
-  Router.push('/');
 
+function HandleClick(router:any) {
+  router.push('/');
 }
 
-const NavBar=()=>(
-  
-<div className="mx-auto w-auto fix top-0 z-500">
-    <div className="bg-slate-700 h-auto  flex flex-col z-100 items-center text-2xl font-semibold text-white pb-10 sm:flex-row justify-evenly">
-    {/*NavBar Links */}
-        
-        <Link href='#about' onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
-        <Link href="#skills" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
-        <Link href="#education" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
-        <Link href="#interests" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
-        <Link href="#goals" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
+const NavBar = () => {
+  const router = useRouter();
 
-      </div></div>)
-  
+  return (
+    <div className="mx-auto w-auto fix top-0 z-500">
+      <div className="bg-slate-700 h-auto  flex flex-col z-100 items-center text-2xl font-semibold text-white pb-10 sm:flex-row justify-evenly">
+        {/*NavBar Links */}
+        <Link href="#about" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
+        <Link href="#skills" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
+        <Link href="#education" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
+        <Link href="#interests" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
+        <Link href="#goals" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
