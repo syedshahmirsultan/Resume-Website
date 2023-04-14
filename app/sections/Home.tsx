efa41949158f590f7053fa16dddbb070d1b7300e
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-14 21:18:37 +0500

    Update Home.tsx

--- app/sections/Home.tsx
+++ app/sections/Home.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 function Home() {
   return (
-    <section id="home" className='h-[900px] scroll-smooth w-12/12  bg-cover bg-black/90 sec'>
-            <div className=" py-[100px] lg:py-[100px] xl:py-[250px]  px-[80px] flex flex-col shrink-0 xl:flex-row mx-auto  justify-evenly items-center">
+    <section className='h-[900px] scroll-smooth w-12/12  bg-cover bg-black/90 sec'>
+            <div id="home" className=" py-[100px] lg:py-[100px] xl:py-[250px]  px-[80px] flex flex-col shrink-0 xl:flex-row mx-auto  justify-evenly items-center">
                 {/*Name and Image*/}
       <div className="space-x-12 text-left"><h2 className="text-white text-6xl md:text-7xl">Hi,I'm <strong className="text-teal-600 text-6xl md:text-7xl">Syed Shahmir Sultan</strong>,a  </h2>
         <h1 className="text-[65px] md:text-7xl lg:text-8xl font-extrabold text-white mt-10 -ml-[50px]">Web Developer</h1></div>
