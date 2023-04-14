ef3ba63a60b36fcf25186d354963f4957eccc4c5
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-14 21:21:36 +0500

    Update Goals.tsx

--- app/sections/Goals.tsx
+++ app/sections/Goals.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Goals = () => {
   return (
-    <section className="scroll-smooth text-gray-600">
-        <div id="goals"><h1 className="text-7xl align-left lg:align-center  font-semibold mt-[100px] sm:mt-[250px] mb-[80px] text-center md:mt-[200px] lg:mt-[200px]">
+    <section id="goals" className="scroll-smooth text-gray-600">
+        <div><h1 className="text-7xl align-left lg:align-center  font-semibold mt-[100px] sm:mt-[250px] mb-[80px] text-center md:mt-[200px] lg:mt-[200px]">
           Goals </h1>
           {/* Future plans */}
           <div className="mr-0 2xl:w-2/2 w-2/2 h-auto bg-gray-200 2xl:mr-[200px] 2xl:ml-[200px] xl:pr-10 pb-10"><p className="text-3xl ml-10  text-gray-900 font-semibold pt-16 pr-4">My future plans involve building the largest IT service company of Pakistan that provides innovative solutions to clients around the world. I believe that technology can transform businesses and make people's lives easier, and I want to be at the forefront of that transformation.
