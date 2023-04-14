a166cea955fd83d86eeb70b03147981324b8fc40
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-14 20:47:36 +0500

    Update NavBar.tsx

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-function handleClick() {
-  const router = useRouter();
-  router.push('/');
+function HandleClick() {
+  const Router = useRouter();
+  Router.push('/');
 
 }
 
@@ -14,11 +14,11 @@ const NavBar=()=>(
     <div className="bg-slate-700 h-auto  flex flex-col z-100 items-center text-2xl font-semibold text-white pb-10 sm:flex-row justify-evenly">
     {/*NavBar Links */}
         
-        <Link href='#about' onClick={handleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
-        <Link href="#skills" onClick={handleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
-        <Link href="#education" onClick={handleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
-        <Link href="#interests" onClick={handleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
-        <Link href="#goals" onClick={handleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
+        <Link href='#about' onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
+        <Link href="#skills" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
+        <Link href="#education" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
+        <Link href="#interests" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
+        <Link href="#goals" onClick={HandleClick} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
 
       </div></div>)
   
