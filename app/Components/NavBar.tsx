7b7a49768c99852f6aca526fc3efc12a789fe5e9
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-22 06:04:00 +0500

    Update NavBar.tsx

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -25,74 +25,7 @@ const NavBar=()=>(
 
 export default NavBar;
 
-// import React from 'react';
-// import Link from 'next/link';
-// import { useRouter } from 'next/router';
 
-// function HandleClick(router:any) {
-//   router.push('/');
-// }
-
-
-
-// const NavBar = () => {
-//   const router = useRouter();
-
-//   return (
-//     <div className="mx-auto w-auto fix top-0 z-500">
-//       <div className="bg-slate-700 h-auto  flex flex-col z-100 items-center text-2xl font-semibold text-white pb-10 sm:flex-row justify-evenly">
-//         {/*NavBar Links */}
-//         <Link href="#about" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
-//         <Link href="#skills" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
-//         <Link href="#education" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
-//         <Link href="#interests" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
-//         <Link href="#goals" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NavBar;
-
-
-
-// import { useRouter } from 'next/router';
-// import Link from 'next/link';
-
-// function withNextRouter(Component:any) {
-//   return function WrappedComponent(props:any) {
-//     const router = useRouter();
-
-  
-//     if (!router.isReady) {
-//       return null;
-//     }
-
-//     return <Component {...props} />;
-//   };
-// }
-// function HandleClick(router:any) {
-//   router.push('/');
-// }
-
-// function NavBar() {
-//   const router = useRouter();
-
-//   return (
-//     <div className="mx-auto w-auto fix top-0 z-500">
-//       <div className="bg-slate-700 h-auto  flex flex-col z-100 items-center text-2xl font-semibold text-white pb-10 sm:flex-row justify-evenly">
-//         {/*NavBar Links */}
-//         <Link href="#about" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">About Me</Link>
-//         <Link href="#skills" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Skills</Link>
-//         <Link href="#education" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110 ">Education</Link>
-//         <Link href="#interests" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Interests</Link>
-//         <Link href="#goals" onClick={() => HandleClick(router)} className="scroll-smooth hover:text-teal-600 mt-4 hover:scale-110">Goals</Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default withNextRouter(NavBar);
 
 
 
