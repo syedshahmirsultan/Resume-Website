03a13ca54d33268cd1c8599f02e528de5d389f48
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-05-13 12:41:37 +0500

    Update Education.tsx

--- app/sections/Education.tsx
+++ app/sections/Education.tsx
@@ -5,7 +5,7 @@ function Education() {
     <section id="education" className="mt-[-150px] scroll-smooth sm:mt-[-150px] md:mt-[-200px] lg:mt-[100px] xl:mt-[-100px] 2xl:mt-[-100px] md:ml-[100px] md:mr-[100px] ">
       <div>
       <h1 className="text-7xl align-left lg:align-center text-gray-600 font-semibold mt-[300px] mb-[120px] text-center md:mt-[400px] lg:mt-[200px]">Education</h1>
-      <div className="bg-gray-200 w-auto mt-[50px] mb-[10px] lg:mb-2  md:w-2/2 lg:w-auto h-auto border-2 border-gray-200 md:mt-2 shadow-md xl:ml-[150px] xl:p-[50px]">
+      <div className="bg-gray-200 w-auto mt-[50px] mb-[10px] lg:mb-2  md:w-2/2 lg:w-auto h-auto border-2 border-gray-200 md:mt-2 shadow-md xl:ml-[100px] xl:p-[50px]">
         {/* Education */}
       <p className="text-3xl font-semibold text-gray-900 mt-10 mb-4 ml-2">Student of Grade 9th in Commodores Learning Foundation (CLF) Islamabad, Pakistan</p>
       </div>
